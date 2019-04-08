122227df475b74df8906e2a95dd5ace8aa0c0a98
adrianmiriuta <adrian.miriuta@gmail.com>
2019-04-08 13:58:28 +0200

    cleanup

--- src/main/main.c
+++ src/main/main.c
@@ -884,39 +884,19 @@ void zc_found_routine(){
   lastzctime = thiszctime;
 }
 
-/* USER CODE END PFP */
 
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-int main(void)
-{
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration----------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+int main(void) {
+  // reset  peripherals, init flash, init systick
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
+  // configure the system clock
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
   HAL_FLASH_Unlock();
   EE_Init();
-//  HAL_Delay(10);
-/* USER CODE END SysInit */
+  // HAL_Delay(10);
 
-  /* Initialize all configured peripherals */
-
+  // initialize peripherals
   ledInit();
   MX_DMA_Init();
   MX_ADC_Init();
@@ -927,7 +907,6 @@ int main(void)
   MX_TIM15_Init();
   MX_IWDG_Init();
 
-  /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
@@ -935,12 +914,10 @@ int main(void)
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
   HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_3);
 
-
   HAL_TIM_Base_Start_IT(&htim2);
   HAL_TIM_Base_Start(&htim3);
-  //  HAL_Delay(500);
+  // HAL_Delay(500);
   playStartupTune();
-
 
   while (HAL_TIM_OC_Start_IT(&htim1, TIM_CHANNEL_4) != HAL_OK);
   HAL_TIM_IC_Start_DMA(&htim15, TIM_CHANNEL_1, dma_buffer, 64);
@@ -962,40 +939,36 @@ int main(void)
     bi_direction = 1;
     slow_decay = 0;                       // for complementary pwm , 0 for diode freewheeling
     brake = 0;                           // apply full motor brake on stop
-    //	 start_power = 150;
+    // start_power = 150;
     prop_brake = 1;
     prop_brake_strength = 900;
   }
-
   if (vehicle_mode == 4) {                 // rc car 50 percent brake on reverse.
     bi_direction = 1;
     slow_decay = 0;                         // for complementary pwm , 0 for diode freewheeling
     brake = 0;                             // apply full motor brake on stop
-    //		 start_power = 150;
+    // start_power = 150;
     prop_brake = 1;
     prop_brake_strength = 800;
   }
-
 
   if(bi_direction) {
     newinput = 1001;
     //	start_power = 175;
   }
 
-  TIM1->CCR1 = 1;                       // set duty cycle to 50 out of 768 to start.
+  // set duty cycle to 50 out of 768 to start.
+  TIM1->CCR1 = 1;
   TIM1->CCR2 = 1;
   TIM1->CCR3 = 1;
-
   TIM1->CCR4 = 800;
-  /* USER CODE END 2 */
 
-  /* Infinite loop */
+  // main loop
   while (true) {
     LED_OFF(LED0);
     LED_OFF(LED1);
 
-    count++;
-    if (count > 100000) {
+    if (++count > 100000) {
       count = 0;
     }
 
@@ -1007,27 +980,21 @@ int main(void)
     if (control_loop_count > 1) {
       control_loop_count = 0;
 
-
-
-//	      1-5: beep (1= low freq. 5 = high freq.)
-//	      6: ESC info request (FW Version and SN sent over the tlm wire)
-//	      7: rotate in one direction
-//	      8: rotate in the other direction
-//	      9: 3d mode off
-//	      10: 3d mode on
-//	      11: ESC settings request (saved settings over the TLM wire) (planed but not there yet)
-//	      12: save Settings
-
+//  1-5: beep (1= low freq. 5 = high freq.)
+//  6: ESC info request (FW Version and SN sent over the tlm wire)
+//  7: rotate in one direction
+//  8: rotate in the other direction
+//  9: 3d mode off
+//  10: 3d mode on
+//  11: ESC settings request (saved settings over the TLM wire) (planed but not there yet)
+//  12: save Settings
 
       if (dshotcommand > 0) {
-
-
         if (dshotcommand == 2) {
           playInputTune();
         }
         if (dshotcommand == 21) {
           forward =  dir_reversed;
-
         }
         if (dshotcommand == 20) {         // forward = 1 if dir_reversed = 0
           forward = 1 - dir_reversed;
@@ -1052,15 +1019,12 @@ int main(void)
         if (dshotcommand == 12) {
           storeEEpromConfig();
           while(1) { // resets esc as iwdg times out
-
           }
         }
       }
 
-
       if (bi_direction == 1 && (proshot == 0 && dshot == 0)) {
         //char oldbrake = brake;
-
         if ( newinput > 1100 ) {
           if (forward == dir_reversed) {
             adjusted_input = 0;
@@ -1070,13 +1034,11 @@ int main(void)
           }
 
           if (prop_brake_active == 0) {
-
             adjusted_input = (newinput - 1050)*3;
             //	tempbrake = 0;
             //	}
           }
         }
-
 
         if (newinput < 800) {
           if (forward == (1 - dir_reversed)) {
@@ -1084,17 +1046,12 @@ int main(void)
             adjusted_input = 0;
             forward = dir_reversed;
             //	HAL_Delay(1);
-
           }
 
           if (prop_brake_active == 0) {
             adjusted_input = (800 - newinput) * 3;
-
           }
           //	tempbrake = 0;
-
-
-
         }
 
         if (zctimeout >= zc_timeout_threshold) {
@@ -1112,7 +1069,7 @@ int main(void)
           prop_brake_active = 0;
         }
 
-      }else if((proshot || dshot ) && bi_direction) {
+      } else if((proshot || dshot ) && bi_direction) {
         if ( newinput > 1097 ) {
 
           if (forward == dir_reversed) {
@@ -1130,8 +1087,7 @@ int main(void)
         if ((newinput > 1047 && newinput < 1098 ) || newinput <= 120) {
           adjusted_input = 0;
         }
-
-      }else{
+      } else {
         adjusted_input = newinput;
       }
 
@@ -1140,26 +1096,21 @@ int main(void)
         adjusted_input = 2000;
       }
 
-
       if (adjusted_input - input > 25) {
         input = input + 5;
-      }else{
+      } else {
         input = adjusted_input;
       }
 
       if (adjusted_input <= input) {
-
         input = adjusted_input;
-
       }
-
     }
 
     advancedivisor = map((commutation_interval),100,5000, 2, 20);
     if (inputSet == 0) {
       HAL_Delay(10);
       detectInput();
-
     }
 
     if (!armed) {
@@ -1175,7 +1126,6 @@ int main(void)
         armedcount = 0;
       }
     }
-
 
     if ((input > 47)&&(armed == 1)) {
       prop_brake_active = 0;
@@ -1200,16 +1150,13 @@ int main(void)
           duty_cycle = 44;
         }
 
-
-        TIM1->CCR1 = duty_cycle;                      // set duty cycle to 50 out of 768 to start.
+        // set duty cycle to 50 out of 768 to start.
+        TIM1->CCR1 = duty_cycle;
         TIM1->CCR2 = duty_cycle;
         TIM1->CCR3 = duty_cycle;
         //	TIM1->CCR4 = duty_cycle;
       }
-
     }
-//
-
 
     signaltimeout++;
     if (signaltimeout > signal_timeout_threshold ) {
@@ -1217,7 +1164,6 @@ int main(void)
       armed = 0;
       armedcount = 0;
       error = 1;
-
       //	  duty_cycle = 0;          //mid point
     }
 
@@ -1234,16 +1180,14 @@ int main(void)
         //	HAL_COMP_Stop_IT(&hcomp1);
       }
 
-
-
-
       if(prop_brake && prop_brake_active) {
         //	prop_brake_active = 1;
         duty_cycle = prop_brake_strength;
         proBrake();
       }
 
-      TIM1->CCR1 = duty_cycle;                        // set duty cycle to 50 out of 768 to start.
+      // set duty cycle to 50 out of 768 to start.
+      TIM1->CCR1 = duty_cycle;
       TIM1->CCR2 = duty_cycle;
       TIM1->CCR3 = duty_cycle;
 
@@ -1257,16 +1201,15 @@ int main(void)
     if (bemf_counts < 100 || commutation_interval > 10000) {
       filter_delay = 15;
       filter_level = 10;
-    }else{
+    } else {
       filter_level = 3;
       filter_delay = 3;
     }
-    if(commutation_interval < 200 && duty_cycle > 500) {
 
+    if(commutation_interval < 200 && duty_cycle > 500) {
       filter_delay = 1;
       filter_level = 0;
     }
-
 
     if (started == 1) {
       if (running == 0) {
@@ -1274,7 +1217,7 @@ int main(void)
         upthreshold = 2;
         threshold = 2;
         zctimeout =0;
-        startMotor(); // safety on for input testing   ************************************************
+        startMotor(); // safety on for input testing
       }
     }
 
@@ -1290,28 +1233,13 @@ int main(void)
       bad_commutation = 0;
       sensorless = 0;
       HAL_COMP_Stop_IT(&hcomp1);
-//			if(tempbrake){
-//
-//			}else{
-//			allOff();
-//			}
+
       running = 0;
       //		commutation_interval = 0;
       zctimeout = zc_timeout_threshold + 1;
       duty_cycle = 0;
     }
-
-
-
-
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-//			}
-//		}
   }
-  /* USER CODE END 3 */
-
 }
 
 
