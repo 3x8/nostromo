32a8b3d92a9506bda6a79b590388e7f5d12e8278
adrianmiriuta <adrian.miriuta@gmail.com>
2019-04-26 07:29:35 +0200

    cleanup

--- src/main/common/common.h
+++ src/main/common/common.h
@@ -2,5 +2,9 @@
 
 #include "main.h"
 
+#define MIN(a,b) __extension__ ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
+#define MAX(a,b) __extension__ ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
+#define ABS(x) __extension__ ({ __typeof__ (x) _x = (x); _x > 0 ? _x : -_x; })
+
 uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max);
 uint32_t constrain(uint32_t amt, uint32_t low, uint32_t high);
