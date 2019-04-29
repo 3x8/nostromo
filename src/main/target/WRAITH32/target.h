3f88f0da7a7ff8880ff597ef66a6b8c73b62d1e4
adrianmiriuta <adrian.miriuta@gmail.com>
2019-04-29 13:51:08 +0200

    cleanup ok

--- src/main/target/WRAITH32/target.h
+++ src/main/target/WRAITH32/target.h
@@ -5,84 +5,51 @@
 
 // PA15 --- LED RED ,Ok
 #define RED_GPIO       GPIOA
-#define RED_PIN        LL_GPIO_PIN_15
+#define RED_PIN        GPIO_PIN_15
 #define RED_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOA
 
 // PB3  --- LED GREEN ,Ok
 #define GREEN_GPIO       GPIOB
-#define GREEN_PIN        LL_GPIO_PIN_3
+#define GREEN_PIN        GPIO_PIN_3
 #define GREEN_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
 
 // PB4  --- LED BLUE ,Ok
 #define BLUE_GPIO       GPIOB
-#define BLUE_PIN        LL_GPIO_PIN_4
+#define BLUE_PIN        GPIO_PIN_4
 #define BLUE_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
 
 // PA10 --- Phase_A_HI ,Ok
 #define A_FET_HI_GPIO   GPIOA
-#define A_FET_HI_PIN    LL_GPIO_PIN_10
+#define A_FET_HI_PIN    GPIO_PIN_10
 
 // PA9 --- Phase_B_HI ,Ok
 #define B_FET_HI_GPIO   GPIOA
-#define B_FET_HI_PIN    LL_GPIO_PIN_9
+#define B_FET_HI_PIN    GPIO_PIN_9
 
 // PA8 --- Phase_C_HI ,Ok
 #define C_FET_HI_GPIO   GPIOA
-#define C_FET_HI_PIN    LL_GPIO_PIN_8
+#define C_FET_HI_PIN    GPIO_PIN_8
 
 // PB1 --- Phase_A_LO ,Ok
 #define A_FET_LO_GPIO   GPIOB
-#define A_FET_LO_PIN    LL_GPIO_PIN_1
+#define A_FET_LO_PIN    GPIO_PIN_1
 
 // PB0 --- Phase_B_LO ,Ok
 #define B_FET_LO_GPIO   GPIOB
-#define B_FET_LO_PIN    LL_GPIO_PIN_0
+#define B_FET_LO_PIN    GPIO_PIN_0
 
 // PA7 --- Phase_C_LO ,Ok
 #define C_FET_LO_GPIO   GPIOA
-#define C_FET_LO_PIN    LL_GPIO_PIN_7
+#define C_FET_LO_PIN    GPIO_PIN_7
 
 // comparator ,Ok
 #define COMPARATOR_PHASE_A       COMP_INVERTINGINPUT_DAC1
 #define COMPARATOR_PHASE_B       COMP_INVERTINGINPUT_DAC2
 #define COMPARATOR_PHASE_C       COMP_INVERTINGINPUT_IO1
 
-// ADC
+// ADC ,Ok
 #define ADC_VOLTAGE               ADC_CHANNEL_3
+#define GPIO_PIN_VOLTAGE          GPIO_PIN_3
 #define ADC_CURRENT               ADC_CHANNEL_6
-#define ADC_TEMPERATURE           LL_ADC_CHANNEL_TEMPSENSOR
-
-//ToDo
-// Sequence number of ADC each particular channel (the order in which they are scanned)
-#define ADC_SEQ_TEMPERATURE       0
-#define ADC_SEQ_PHASE_A           1
-#define ADC_SEQ_VOLTAGE           3
-#define ADC_SEQ_PHASE_B           4
-#define ADC_SEQ_PHASE_C           5
-#define ADC_SEQ_CURRENT           6
-
-#define ADC_CHAN_TEMPERATURE      LL_ADC_CHANNEL_TEMPSENSOR // CH16
-#define ADC_CHAN_VOLTAGE          LL_ADC_CHANNEL_3
-#define ADC_CHAN_CURRENT          LL_ADC_CHANNEL_6
-//#define ADC_CHAN_PHASE_A          LL_ADC_CHANNEL_0
-//#define ADC_CHAN_PHASE_B          LL_ADC_CHANNEL_4
-//#define ADC_CHAN_PHASE_C          LL_ADC_CHANNEL_5
-
-#define TARGET_ADC_CHANNEL_MASK ( ADC_CHAN_PHASE_A | \
-                                  ADC_CHAN_PHASE_B | \
-                                  ADC_CHAN_PHASE_C | \
-                                  ADC_CHAN_TEMPERATURE | \
-                                  ADC_CHAN_VOLTAGE | \
-                                  ADC_CHAN_CURRENT )
-
-#define GPIO_PIN_PHASE_A          LL_GPIO_PIN_0
-#define GPIO_PIN_PHASE_B          LL_GPIO_PIN_4
-#define GPIO_PIN_PHASE_C          LL_GPIO_PIN_5
-#define GPIO_PIN_VOLTAGE          LL_GPIO_PIN_3
-#define GPIO_PIN_CURRENT          LL_GPIO_PIN_6
-
-#define TARGET_ADC_PIN_MASK     ( GPIO_PIN_PHASE_A | \
-                                  GPIO_PIN_PHASE_B | \
-                                  GPIO_PIN_PHASE_C | \
-                                  GPIO_PIN_VOLTAGE | \
-                                  GPIO_PIN_CURRENT )
+#define GPIO_PIN_CURRENT          GPIO_PIN_6
+#define ADC_TEMPERATURE           ADC_CHANNEL_TEMPSENSOR
