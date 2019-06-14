9de30b18b173ba73738e3e55d72dd2b5de7a69c5
adrianmiriuta <adrian.miriuta@gmail.com>
2019-06-14 08:48:14 +0200

    DYS FET dead time ???

--- src/main/target/DYS35ARIA/target.h
+++ src/main/target/DYS35ARIA/target.h
@@ -8,7 +8,7 @@
 //#define INPUT_AUTODETECT
 
 //ToDo
-#define HBRIDGE_DEAD_TIME 0 // in 22ns cycles (FD6288 builtin 200ns)
+#define HBRIDGE_DEAD_TIME 9 // in 22ns cycles (FD6288 builtin 200ns)
 #define HBRIDGE_MAX_CURRENT 3011 // ~54A
 #define HBRIDGE_MAX_TEMPERATURE 1234 // not used
 #define TIMER1_INIT_PERIOD  997 // ~24.1 kHz
