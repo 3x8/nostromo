6325eeb4ec598bfa63f115845c79b50bdab2e6e6
adrianmiriuta <adrian.miriuta@gmail.com>
2019-07-12 11:29:18 +0200

    cleanup

--- src/main/target/WRAITH32V2/target.h
+++ src/main/target/WRAITH32V2/target.h
@@ -14,42 +14,42 @@
 #define TIMER1_INIT_PERIOD  977 // max linearity //797~30.03kHz //911~26.31kHz //977~24.4kHz //997~24.1kHz
 #define MOTOR_START_THRESHOLD 13
 
-// PA15 --- LED RED ,Ok
+// LED RED ,Ok
 #define RED_GPIO       GPIOA
 #define RED_PIN        GPIO_PIN_15
 #define RED_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOA
 
-// PB3  --- LED GREEN ,Ok
+// LED GREEN ,Ok
 #define GREEN_GPIO       GPIOB
 #define GREEN_PIN        GPIO_PIN_3
 #define GREEN_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
 
-// PB4  --- LED BLUE ,Ok
+// LED BLUE ,Ok
 #define BLUE_GPIO       GPIOB
 #define BLUE_PIN        GPIO_PIN_4
 #define BLUE_PERIPHERAL LL_AHB1_GRP1_PERIPH_GPIOB
 
-// PA10 --- Phase_A_HI ,Ok
+// Phase_A_HI ,Ok
 #define A_FET_HI_GPIO   GPIOA
 #define A_FET_HI_PIN    GPIO_PIN_10
 
-// PA9 --- Phase_B_HI ,Ok
+// Phase_B_HI ,Ok
 #define B_FET_HI_GPIO   GPIOA
 #define B_FET_HI_PIN    GPIO_PIN_9
 
-// PA8 --- Phase_C_HI ,Ok
+// Phase_C_HI ,Ok
 #define C_FET_HI_GPIO   GPIOA
 #define C_FET_HI_PIN    GPIO_PIN_8
 
-// PB1 --- Phase_A_LO ,Ok
+// Phase_A_LO ,Ok
 #define A_FET_LO_GPIO   GPIOB
 #define A_FET_LO_PIN    GPIO_PIN_1
 
-// PB0 --- Phase_B_LO ,Ok
+// Phase_B_LO ,Ok
 #define B_FET_LO_GPIO   GPIOB
 #define B_FET_LO_PIN    GPIO_PIN_0
 
-// PA7 --- Phase_C_LO ,Ok
+// Phase_C_LO ,Ok
 #define C_FET_LO_GPIO   GPIOA
 #define C_FET_LO_PIN    GPIO_PIN_7
 
