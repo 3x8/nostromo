f66103afdfb16a0aba16db351ad6d2d13bd58dd3
adrianmiriuta <adrian.miriuta@gmail.com>
2019-07-15 06:07:58 +0200

    version up

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -2,5 +2,5 @@
 
 #define FW_FIRMWARE_NAME            "nostromo"
 #define FW_VERSION_MAJOR            1
-#define FW_VERSION_MINOR            2
-#define FW_VERSION_PATCH_LEVEL      2
+#define FW_VERSION_MINOR            3
+#define FW_VERSION_PATCH_LEVEL      0
