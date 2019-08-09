66a8772d4e9dc26761acc78d3ef9e1f811af55ab
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-08-09 08:26:35 +0200

    WRAITH32 target

--- src/main/target/WRAITH32/target.h
+++ src/main/target/WRAITH32/target.h
@@ -6,13 +6,15 @@
 //#define DEBUG_MOTOR_TIMING
 //#define DEBUG_INPUT_PROSHOT
 //#define DEBUG_INPUT_AUTODETECT
+//#define DEBUG_DATA_UART
+//#define DEBUG_MS_TIMER
 
 // ToDo
-#define HBRIDGE_DEAD_TIME 0 // in 22ns cycles (FD6288 builtin 200ns)
-#define HBRIDGE_MAX_CURRENT 571 // x0.1A
-#define HBRIDGE_MAX_TEMPERATURE 77 // °C
-#define TIMER1_INIT_PERIOD  977 // max linearity //797~30.03kHz //911~26.31kHz //977~24.4kHz //997~24.1kHz
-#define MOTOR_START_THRESHOLD 14
+#define HBRIDGE_DEAD_TIME       0     // (in 21.26ns cycles ~47.017MHz) (FD6288 has a builtin 200ns deadtime)
+#define HBRIDGE_MAX_CURRENT     5710  // (in 10mA steps)
+#define HBRIDGE_MAX_TEMPERATURE 77    // °C
+#define TIMER1_INIT_PERIOD      977   // max linearity //797~30.03kHz //911~26.31kHz //977~24.4kHz //997~24.1kHz
+#define MOTOR_START_THRESHOLD   14
 
 // input ,Ok
 #define INPUT_GPIO      GPIOA
@@ -25,7 +27,7 @@
 #define LED_GREEN_PIN   GPIO_PIN_3
 #define LED_BLUE_GPIO   GPIOB
 #define LED_BLUE_PIN    GPIO_PIN_4
-#define LED_MASK        LL_AHB1_GRP1_PERIPH_GPIOA | LL_AHB1_GRP1_PERIPH_GPIOB | LL_AHB1_GRP1_PERIPH_GPIOB
+#define LED_MASK        LL_AHB1_GRP1_PERIPH_GPIOA | LL_AHB1_GRP1_PERIPH_GPIOB
 
 // H_BRIDGE ,Ok
 #define A_FET_HI_GPIO   GPIOA
@@ -42,41 +44,34 @@
 #define C_FET_LO_PIN    GPIO_PIN_7
 
 // comparator ,Ok
-#define COMPARATOR_PHASE_A       COMP_INVERTINGINPUT_IO1
-#define COMPARATOR_PHASE_B       COMP_INVERTINGINPUT_DAC1
-#define COMPARATOR_PHASE_C       COMP_INVERTINGINPUT_DAC2
-#define COMPARATOR_COMMON        COMP_NONINVERTINGINPUT_IO1
-#define COMPARATOR_MASK          GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5
+#define COMPARATOR_PHASE_A      COMP_INVERTINGINPUT_IO1
+#define COMPARATOR_PHASE_B      COMP_INVERTINGINPUT_DAC1
+#define COMPARATOR_PHASE_C      COMP_INVERTINGINPUT_DAC2
+#define COMPARATOR_COMMON       COMP_NONINVERTINGINPUT_IO1
+#define COMPARATOR_MASK         GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5
 
-// ADC ,Ok
-#define ADC_VOLTAGE              ADC_CHANNEL_3
-#define ADC_CURRENT              ADC_CHANNEL_6
-#define ADC_TEMPERATURE          ADC_CHANNEL_TEMPSENSOR
-#define ADC_MASK                 GPIO_PIN_3 | GPIO_PIN_6
+// adc ,Ok
+#define ADC_VOLTAGE             ADC_CHANNEL_3
+#define ADC_CURRENT             ADC_CHANNEL_6
+#define ADC_TEMPERATURE         ADC_CHANNEL_TEMPSENSOR
+#define ADC_MASK                GPIO_PIN_3 | GPIO_PIN_6
 
-// corr ,
-#define ADC_VOLTAGE_OFFSET  1
-#define ADC_VOLTAGE_FACTOR  0.0601
-#define ADC_CURRENT_OFFSET  80
-#define ADC_CURRENT_FACTOR  0.23933
+// adc calibration ,Ok
+#define ADC_VOLTAGE_OFFSET      40.3128760529
+#define ADC_VOLTAGE_FACTOR      0.601684717208
+#define ADC_CURRENT_OFFSET      0  // 0 -> auto offset
+#define ADC_CURRENT_FACTOR      1.87
+#define ADC_CONSUMPTION_FACTOR  0.00028
 #define ADC_TEMPERATURE_OFFSET  95.05
-#define ADC_TEMPERATURE_FACTOR -0.04
+#define ADC_TEMPERATURE_FACTOR  -0.04
 
-// UART
-#define USART                           USART1
-#define USART_IRQn                      USART1_IRQn
-#define USART_IRQHandler                USART1_IRQHandler
-
-#define USART_TX_PIN                    LL_GPIO_PIN_6
-#define USART_TX_GPIO_PORT              GPIOB
-#define USART_TX_GPIO_CLK               LL_AHB1_GRP1_PERIPH_GPIOB
-#define USART_TX_AF                     LL_GPIO_AF_0
-
-#define USART_RX_PIN                    LL_GPIO_PIN_7
-#define USART_RX_GPIO_PORT              GPIOB
-#define USART_RX_GPIO_CLK               LL_AHB1_GRP1_PERIPH_GPIOB
-#define USART_RX_AF                     LL_GPIO_AF_0
-
-#define USART_TDR_ADDRESS               0x40013828
-
-#define USART_TX_DMA_CHANNEL            LL_DMA_CHANNEL_2
+// telemetry
+#define USART                   USART1
+#define USART_IRQn              USART1_IRQn
+#define USART_IRQHandler        USART1_IRQHandler
+#define USART_TX_PIN            LL_GPIO_PIN_6
+#define USART_TX_GPIO_PORT      GPIOB
+#define USART_TX_GPIO_CLK       LL_AHB1_GRP1_PERIPH_GPIOB
+#define USART_TX_AF             LL_GPIO_AF_0
+#define USART_TDR_ADDRESS       0x40013828
+#define USART_TX_DMA_CHANNEL    LL_DMA_CHANNEL_2
