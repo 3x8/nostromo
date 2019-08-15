8a47ee787c2a3fc8d290ad07584c5dafedc683e6
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-08-15 09:56:22 +0200

    debug

--- src/main/target/TYPHOON32V2/target.h
+++ src/main/target/TYPHOON32V2/target.h
@@ -3,8 +3,8 @@
 // debug
 #define _DEBUG_
 //#define DEBUG_CYCLETIME_MAINLOOP // ~500us
-//#define DEBUG_MOTOR_TIMING
-#define DEBUG_INPUT_PROSHOT
+#define DEBUG_MOTOR_TIMING
+//#define DEBUG_INPUT_PROSHOT
 //#define DEBUG_INPUT_AUTODETECT
 //#define DEBUG_DATA_UART
 //#define DEBUG_MS_TIMER
