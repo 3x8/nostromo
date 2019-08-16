afabf104536d50186336d404f6d9aa1dc24977fa
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-08-16 20:25:33 +0200

    no debug

--- src/main/target/TYPHOON32V2/target.h
+++ src/main/target/TYPHOON32V2/target.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // debug
-#define _DEBUG_
+//#define _DEBUG_
 //#define DEBUG_CYCLETIME_MAINLOOP // ~500us
-#define DEBUG_MOTOR_TIMING
+//#define DEBUG_MOTOR_TIMING
 //#define DEBUG_INPUT_PROSHOT
 //#define DEBUG_INPUT_AUTODETECT
 //#define DEBUG_DATA_UART
