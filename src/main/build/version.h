d6972bc9af1157a3a4a5d58091c8d0119aeae3e1
adrianmiriuta <adrian.miriuta@gmail.com>
2019-11-12 13:18:25 +0100

    SUCCEX50AV2 new version

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -2,5 +2,5 @@
 
 #define FW_FIRMWARE_NAME    "nostromo"
 #define FW_VERSION_MAJOR    1
-#define FW_VERSION_MINOR    8
-#define FW_VERSION_PATCH    19
+#define FW_VERSION_MINOR    9
+#define FW_VERSION_PATCH    1
