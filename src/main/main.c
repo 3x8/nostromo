e031063ed2d1193a6141d7a729367edfef93b918
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-12-28 19:45:08 +0100

    proshot test

--- src/main/main.c
+++ src/main/main.c
@@ -24,7 +24,7 @@ int main(void) {
   systemMsTimerInit();
   ledOff();
 
-  kalmanInit(&motorCommutationIntervalFilterState, 5000.0f, 11);
+  kalmanInit(&motorCommutationIntervalFilterState, 5000.0f, 7);
   #if defined(USE_ADC)
     kalmanInit(&adcVoltageFilterState, 1500.0f, 13);
     kalmanInit(&adcCurrentFilterState, 1500.0f, 13);
@@ -111,7 +111,7 @@ int main(void) {
           motor.BemfZeroCrossTimestamp = 0;
           motor.BemfCounter = 0;
           motor.Running = false;
-          kalmanInit(&motorCommutationIntervalFilterState, 2500.0f, 13);
+          kalmanInit(&motorCommutationIntervalFilterState, 5000.0f, 7);
         }
 
         // motor start
