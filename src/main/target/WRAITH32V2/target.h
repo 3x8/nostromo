d91448833d6f2bb40b2b5c93ba341d18fe4cb5b9
Adrian Miriuta <adrian.miriuta@gmail.com>
2019-12-28 20:17:41 +0100

    proshot test

--- src/main/target/WRAITH32V2/target.h
+++ src/main/target/WRAITH32V2/target.h
@@ -14,7 +14,7 @@
 #define HBRIDGE_DEAD_TIME       0     // (in 21.26ns cycles ~47.017MHz) (FD6288 has a builtin 200ns deadtime)
 #define HBRIDGE_MAX_CURRENT     6719  // (in 10mA steps)
 #define HBRIDGE_MAX_TEMPERATURE 77    // °C
-#define TIMER1_INIT_PERIOD      512  // max linearity
+#define TIMER1_INIT_PERIOD      501  // max linearity
 #define MOTOR_START_THRESHOLD   13
 
 // input ,Ok
