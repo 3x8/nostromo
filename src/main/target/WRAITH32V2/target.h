8caba9039ed774385b67095575209e5c4a1bc182
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-01-08 13:10:28 +0100

    corr

--- src/main/target/WRAITH32V2/target.h
+++ src/main/target/WRAITH32V2/target.h
@@ -83,7 +83,7 @@
 #if !(defined(PWM_FREQUENCY_48kHz))
   #define ADC_CURRENT_FACTOR    1.87
 #else
-  #define ADC_CURRENT_FACTOR    2.11
+  #define ADC_CURRENT_FACTOR    2.34
 #endif
 
 #define ADC_CONSUMPTION_FACTOR  0.00028
