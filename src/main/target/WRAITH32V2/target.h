c1efce4ff704138eb80392dd46a2f76c4640a20c
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-04-14 14:14:55 +0200

    curr limit

--- src/main/target/WRAITH32V2/target.h
+++ src/main/target/WRAITH32V2/target.h
@@ -17,7 +17,7 @@
 
 // hw constants
 #define HBRIDGE_DEAD_TIME       0     // (in 20.833ns cycles at 48MHz) (FD6288 has a builtin 200ns deadtime)
-#define HBRIDGE_MAX_CURRENT     6007  // (in 10mA steps)
+#define HBRIDGE_MAX_CURRENT     6719  // (in 10mA steps)
 #define HBRIDGE_MAX_TEMPERATURE 77    // °C
 #if (!defined(USE_PWM_FREQUENCY_48kHz))
   #define TIMER1_INIT_PERIOD    1001
