52a4b72b6f03efa25119e78e7b98e3b653a734f5
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-06-24 09:36:36 +0200

    eeprom size

--- src/main/drivers/eeprom.h
+++ src/main/drivers/eeprom.h
@@ -4,7 +4,12 @@
 
 #define EEPROM_VERSION 5
 
-#define FLASH_PAGE_COUNT      64
+#if defined(STSPIN32F0)
+  #define FLASH_PAGE_COUNT      32
+#else
+  #define FLASH_PAGE_COUNT      64
+#endif
+
 #define FLASH_EEPROM_SIZE     0x400  // 1x1K page
 #define FLASH_EEPROM_ADDRESS  (0x08000000 + (uint32_t)((FLASH_PAGE_SIZE * FLASH_PAGE_COUNT) - FLASH_EEPROM_SIZE))
 
