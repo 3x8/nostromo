2086f1f652b57896dc0ce8aa63c71497bed6c153
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-07-03 06:20:02 +0200

    cleanup

--- make/mcu/STM32F0.mk
+++ make/mcu/STM32F0.mk
@@ -13,7 +13,7 @@ EXCLUDES         = \
                    stm32f0xx_hal_timebase_tim_template.c \
                    stm32f0xx_hal_msp_template.c
 
-STARTUP_SRC      = startup_$(STM_CHIP)_gcc.s
+STARTUP_SRC      = startup_$(STM_CHIP).s
 DRIVER_SRC      := $(filter-out ${EXCLUDES}, $(DRIVER_SRC))
 
 # Search path and source files for the CMSIS sources
@@ -33,8 +33,7 @@ LD_SCRIPT       = $(LINKER_DIR)/stm32_flash_f0xx_$(FLASH_SIZE)k_boot.ld
 endif
 ARCH_FLAGS      = -mthumb -mcpu=cortex-m0
 
-DEVICE_FLAGS   += -DSTM32F051x8 \
-                  -DUSE_HAL_DRIVER \
+DEVICE_FLAGS   += -DUSE_HAL_DRIVER \
                   -DUSE_FULL_LL_DRIVER
 
 MCU_SRC        := target/system_stm32f0xx.c \
