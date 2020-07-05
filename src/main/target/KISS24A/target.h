cfac814791b9eaffc6148a3c51f678557cd3ff99
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-07-05 11:17:36 +0200

    remove debug

--- src/main/target/KISS24A/target.h
+++ src/main/target/KISS24A/target.h
@@ -4,12 +4,12 @@
 // bootloader does not work !
 
 // debug
-#define _DEBUG_
+//#define _DEBUG_
 //#define DEBUG_CYCLETIME_MAINLOOP
 //#define DEBUG_MOTOR_TIMING
 //#define DEBUG_INPUT_PROSHOT
 //#define DEBUG_INPUT_AUTODETECT
-#define DEBUG_DATA_UART
+//#define DEBUG_DATA_UART
 //#define DEBUG_MS_TIMER
 //#define DEBUG_UART1
 //#define DEBUG_DATA_QUALITY
