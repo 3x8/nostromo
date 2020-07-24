2184f50c143778b5ae153028e8f63c663cd1f745
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-07-24 16:49:37 +0200

    typhoon lower current, kalman lower filtering

--- src/main/main.c
+++ src/main/main.c
@@ -37,8 +37,8 @@ int main(void) {
       medianInit(&adcVoltageFilterState, 8);
       medianInit(&adcCurrentFilterState, 8);
     #else
-      kalmanInit(&adcVoltageFilterState, 1500.0f, 13);
-      kalmanInit(&adcCurrentFilterState, 1500.0f, 13);
+      kalmanInit(&adcVoltageFilterState, 50000.0f, 7);
+      kalmanInit(&adcCurrentFilterState, 50000.0f, 7);
     #endif
   #endif
 
