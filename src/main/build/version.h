34b1e7b8a77c202a33c2c852e3990fca231ac7d6
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-07-26 15:07:38 +0200

    rc 1.18.0

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -2,5 +2,5 @@
 
 #define FW_FIRMWARE_NAME    "nostromo"
 #define FW_VERSION_MAJOR    1
-#define FW_VERSION_MINOR    17
-#define FW_VERSION_PATCH    17
+#define FW_VERSION_MINOR    18
+#define FW_VERSION_PATCH    0
