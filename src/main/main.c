d337bd001b20cac82637462612957a54e7bba477
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-07-26 16:19:29 +0200

    higher median

--- src/main/main.c
+++ src/main/main.c
@@ -34,8 +34,8 @@ int main(void) {
 
   #if (defined(USE_ADC))
     #if (defined(USE_ADC_MEDIAN))
-      medianInit(&adcVoltageFilterState, 8);
-      medianInit(&adcCurrentFilterState, 8);
+      medianInit(&adcVoltageFilterState, 16);
+      medianInit(&adcCurrentFilterState, 16);
     #else
       kalmanInit(&adcVoltageFilterState, 5000.0f, 11);
       kalmanInit(&adcCurrentFilterState, 5000.0f, 11);
