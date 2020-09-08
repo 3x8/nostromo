e5948985977ca7a0fa13b15c2b4d4701e8bb768a
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-09-08 07:14:08 +0200

    size optimized

--- make/source.mk
+++ make/source.mk
@@ -18,21 +18,22 @@ SPEED_OPTIMISED_SRC := $(SPEED_OPTIMISED_SRC) \
                     $(TARGET_DIR_SRC) \
                     main.c \
                     common/common.c \
-                    config/config.c \
                     drivers/adc.c \
                     drivers/led.c \
-                    drivers/eeprom.c \
                     drivers/watchdog.c \
-                    drivers/system.c \
                     drivers/input.c \
                     drivers/motor.c \
                     drivers/uart.c \
                     drivers/telemetry.c
 
-SIZE_OPTIMISED_SRC  := ""
+SIZE_OPTIMISED_SRC  := $(SIZE_OPTIMISED_SRC) \
+                    $(TARGET_DIR_SRC) \
+                    config/config.c \
+                    drivers/eeprom.c \
+                    drivers/system.c
 
 # check if target.mk supplied
-SRC                 := $(STARTUP_DIR)/$(STARTUP_SRC) $(COMMON_SRC) $(SPEED_OPTIMISED_SRC) $(MCU_SRC) $(CMSIS_SRC) $(DRIVER_SRC)
+SRC                 := $(STARTUP_DIR)/$(STARTUP_SRC) $(COMMON_SRC) $(SPEED_OPTIMISED_SRC) $(SIZE_OPTIMISED_SRC) $(MCU_SRC) $(CMSIS_SRC) $(DRIVER_SRC)
 
 #excludes
 SRC                 := $(filter-out ${MCU_EXCLUDES}, $(SRC))
