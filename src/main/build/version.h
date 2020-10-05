028d6a21fa453de1486859e7a1c327dbfc947aea
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-10-05 20:22:53 +0200

    version up

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -2,5 +2,5 @@
 
 #define FW_FIRMWARE_NAME    "nostromo"
 #define FW_VERSION_MAJOR    1
-#define FW_VERSION_MINOR    23
-#define FW_VERSION_PATCH    31
+#define FW_VERSION_MINOR    24
+#define FW_VERSION_PATCH    1
