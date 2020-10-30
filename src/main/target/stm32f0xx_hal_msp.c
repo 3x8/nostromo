c5cf96b1a53e66fcb22134609b7ee85dc94a5c00
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-10-30 07:05:24 +0100

    input pulldown

--- src/main/target/stm32f0xx_hal_msp.c
+++ src/main/target/stm32f0xx_hal_msp.c
@@ -94,7 +94,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* timerHandle) {
 
     GPIO_InitStruct.Pin = INPUT_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(INPUT_GPIO, &GPIO_InitStruct);
 
